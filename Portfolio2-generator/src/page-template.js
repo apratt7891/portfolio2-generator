5bfbb0fea6a624be56a192d9d819d0e35ca842ff
apratt7891 <apratt7891@gmail.com>
2021-05-15 18:04:23 -0600

    add more to prompt

--- Portfolio2-generator/src/page-template.js
+++ Portfolio2-generator/src/page-template.js
@@ -1,4 +1,4 @@
-const generatePage = (name,github) => {
+module.exports = (name, github) => {
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -19,9 +19,5 @@ const generatePage = (name,github) => {
 
 };
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
 
-module.exports = generatePage;
+
